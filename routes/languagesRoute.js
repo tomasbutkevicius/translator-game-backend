cfb26703902fd7069a55e666a6b900f7adbe551c
Tomas <tomas.butkevicius99@gmail.com>
2021-05-05 19:37:26 +0300

    handle errors

--- routes/languagesRoute.js
+++ routes/languagesRoute.js
@@ -15,9 +15,16 @@ const options = {
 
 router.get("/", async (req, res) => {
     request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-    
-        res.send(body);
+      if (error){
+        res.send(error);
+        return;
+      }
+      jsonResponse = JSON.parse(body);
+      if (jsonResponse.error) {
+        res.status(jsonResponse.error.code).send(jsonResponse);
+        return;
+      }
+      res.send(jsonResponse);
     });
 });
 
--- routes/translateRoute.js
+++ routes/translateRoute.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const request = require('request');
+var zlib = require('zlib');
 
 const options = {
-    method: 'POST',
-    url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'accept-encoding': 'application/gzip',
-      'x-rapidapi-key': process.env.KEY,
-      'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
-      useQueryString: true
-    },
-    form: {q: 'Hello, world!', target: 'es', source: 'en'}
-  };
+  method: 'POST',
+  url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'accept-encoding': 'application/gzip',
+    'x-rapidapi-key': process.env.KEY,
+    'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
+    useQueryString: true
+  }
+};
 
 router.post("/", async (req, res) => {
-    options.form = req.body;
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+  options.form = req.body;
+  request(options, function (error, response, body) {
+    if (error) {
+      res.send(error);
+      return;
+    }
+    jsonResponse = JSON.parse(body);
+    if (jsonResponse.error) {
+      res.status(jsonResponse.error.code).send(jsonResponse);
+      return;
+    }
     
-        res.send(body);
-    });
+    res.send(jsonResponse);
+  });
 });
 
 module.exports = router;
